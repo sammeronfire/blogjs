fd63e2e324b92f2845640280825ba866ae98c3ff
Aleksey Sammeroff <lekseyslav@gmail.com>
2020-08-03 16:38:39 +0500

    Added link to article, creating articles, added slugs, editet links thrught the

--- server.js
+++ server.js
@@ -1,18 +1,24 @@
 const express = require('express')
+const mongoose = require('mongoose')
+const Article = require('./models/article')
 const articleRouter = require('./routes/articles')
 const app = express()
 
+mongoose.connect('mongodb://localhost/blog', { 
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true
+})
+
 app.set('view engine', 'ejs')
 
-app.use('/articles', articleRouter)
+app.use(express.urlencoded({ extended: false }))
 
-app.get('/', (req, res) => {
-    const articles = [{
-        title: 'Test Article',
-        createdAt: new Date(),
-        description: 'Test description'
-    }]
+app.get('/', async (req, res) => {
+    const articles = await Article.find().sort({ createdAt: 'desc' })
     res.render('articles/index', { articles: articles })
 })
+
+app.use('/articles', articleRouter)
 
 app.listen(5000)
