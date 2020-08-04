e2f2f98140227af866ce587b579b5105bbce5763
Aleksey Sammeroff <lekseyslav@gmail.com>
2020-08-04 13:33:10 +0500

    It's done

--- routes/articles.js
+++ routes/articles.js
@@ -17,24 +17,34 @@ router.get('/:slug', async (req, res) => {
     res.render('articles/show', {article: article})
 })
 
-router.post('/', async (req, res) => {
-    let article = new Article({
-        title: req.body.title,
-        description: req.body.description,
-        markdown: req.body.markdown
-    })
-    try {
-        article = await article.save()
-        res.redirect(`/articles/${article.slug}`)
-    } catch (e) {
-        console.log(e)
-        res.render('articles/new', { article: article })
-    }
-})
+router.post('/', async (req, res, next) => {
+    req.article = new Article()
+}, saveArticleAndRedirect('new'))
+
+router.put('/:id', async (req, res, next) => {
+    req.article = await Article.findById(req.params.id)
+}, saveArticleAndRedirect('edit'))
 
 router.delete('/:id', async (req, res) => {
     await Article.findByIdAndDelete(req.params.id)
     res.redirect('/')
 })
+
+function saveArticleAndRedirect(path) {
+    return async (req, res) => {
+        let article = req.article
+        article.title = req.body.title
+        article.description = req.body.description
+        article.markdown = req.body.markdown
+
+        try {
+            article = await article.save()
+            res.redirect(`/articles/${article.slug}`)
+        } catch (e) {
+            res.render(`articles/${path}`, {article: article})
+        }
+    }
+}
+
 
 module.exports = router
